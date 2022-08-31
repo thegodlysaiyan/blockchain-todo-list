1f1c97d9181b4de2ca97cd3774719d63fcc5796f
aadarsh_kumar <44476556+thegodlysaiyan@users.noreply.github.com>
2022-08-31 14:19:57 -0700

    Update TodoList.sol

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -23,7 +23,7 @@ contract TodoList {
   );
 
   constructor() public {
-    createTask("Check out dappuniversity.com");
+    createTask("List");
   }
 
   function createTask(string memory _content) public {
